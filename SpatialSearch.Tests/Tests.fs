2de3377a20b96259a94a2890512874d2a44c6bb8
Patrick Kelly <patrick.kelly@sherwin.com>
2017-03-17 12:38:11 -0400

    tests running freal now

--- SpatialSearch.Tests/Tests.fs
+++ SpatialSearch.Tests/Tests.fs
@@ -1,23 +1,23 @@
-﻿
+﻿namespace SpatialSearch.Tests
 
-module SpatialSearch.Tests
-open Xunit
-open System
-open SpatialSearch
-open SpatialSearch.Library
+module LibraryTests = 
+    open Xunit
+    open System
+    open SpatialSearch.Library
+    open FsUnit
 
-[<Fact>]
-let ``location created correctly`` () =
-    let aLocation = location(1,2,3)
-    let expected = {X=1; Y=2; Z=3}
-    Assert.Equal(expected, aLocation)
 
-[<Theory>]
-[<InlineData(1,2)>]
-[<InlineData(2,1)>]
-let ``inOrder places items in order from lowest to highest`` (value1:int, value2:int) =
-    let highest =  Math.Max(value1, value2)
-    let lowest = Math.Min(value1, value2)
-    let ordered = inOrder(value1, value2)
-    Assert.Equal((lowest, highest), ordered)
+    [<Fact>]
+    let ``location created correctly`` () =
+        let aLocation = location(1,2,3)
+        let expected = {X=1; Y=2; Z=3}
+        Assert.Equal(expected, aLocation)
+
+    [<Theory>]
+    [<InlineData(1,2)>]
+    [<InlineData(2,1)>]
+    let ``inOrder places items in order from lowest to highest`` (value1:int, value2:int) =
+        let expectedOrder =  (Math.Min(value1, value2), Math.Max(value1, value2))
+        let actualOrder = inOrder(value1, value2)
+        Assert.Equal(expectedOrder, actualOrder)
     
